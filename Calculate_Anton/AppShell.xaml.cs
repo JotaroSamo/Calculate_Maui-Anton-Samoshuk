e5c2e18af2b05b645b26745e02e01ce7b6d3c503
Dimension-W <Dimension-W@HOME-PC>
2022-10-18 13:18:57 +0300

    Добавьте файлы проекта.

---
+++ Calculate_Anton/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculate_Anton;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Calculate_Anton/Calculate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Anton
+{
+    public static class Calculate
+    {
+        public static string DoCalculation(float val1, float val2, string operatorMath)// АРИФМЕТИЧЕСКИЕ ВЫРАЖЕНИЯ
+        {
+            string result = "0";
+            switch (operatorMath)
+            {
+                case "/":
+                        result = (val1 / val2).ToString();
+                    break;
+                case "-":
+                    result = (val1 - val2).ToString();
+                    break;
+                case "*":
+                    result = (val1 * val2).ToString();
+                    break;
+                case "+":
+                    result = (val1 + val2).ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
